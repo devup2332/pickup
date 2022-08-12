d484943d80d69d50b90436b5b39cd5228b741740
Diego Rojas <devup2332@gmail.com>
2022-08-12 17:00:59 -0500

    master: Setting prettier and eslint with a pre-commit hook

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'standard',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		'quotes': ['error', 'single'],
+		'no-unused-vars': 'error',
+		'semi': ['error', 'always'],
+		'indent': ['error', 'tab'],
+		'quote-props': ['error', 'consistent'],
+		'curly': 'off',
+		'no-tabs': 'off',
+		'comma-dangle': ['off'],
+		'space-before-function-paren': 'off',
+	},
+};
